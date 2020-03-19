8f794d8cabbda539b3ac8d157b3e36dbdcdce2a8
Leonardo Henrique <leonardo.henrique45@gmail.com>
2020-03-18 22:51:37 -0300

    Criando modelo para lista de negociações e Template dinâmico no DOM

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,20 +2,27 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoesView: NegociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes); // inicializa a tabela.
     }
 
     adiciona(event: Event) { 
+        
         event.preventDefault(); // prevenir que a página não seja recarregada.
+        
         const negociacao = new Negociacao(
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao.quantidade + 20);
+
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes); // atualiza a tabela.
     }
 }
