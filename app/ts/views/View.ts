433ea08587ca56eb9231e9eb822de0d0675c1e89
Leonardo Henrique <leonardo.henrique45@gmail.com>
2020-03-18 23:46:35 -0300

    tornando classe abstrata e metodo para obrigar as filhas implementarem

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,6 +1,6 @@
-class View<T> {
+abstract class View<T> {
     private _elemento: Element;
     constructor(selector: string)   { this._elemento = document.querySelector(selector);            }
     update(model: T): void          { this._elemento.innerHTML = this.template(model);              }
-    template(model: T): string      { throw new Error('Necessário implementar o metodo template');  }
+    abstract template(model: T): string;      //{ throw new Error('Necessário implementar o metodo template');  }
 }
